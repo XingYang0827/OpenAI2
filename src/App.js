767840179b4da76a3ddf456d2a4b510ee7e7d06e
XingYang0827 <xingyang0827@gmail.com>
2023-01-05 00:08:14 -0800

    6-2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components'
 import React, { Component } from 'react';
-import { observer } from 'mobx-react'
+import { Provider  } from 'mobx-react'
+import { observer,  } from 'mobx-react'
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import { Router, Switch, Redirect, Route } from "react-router-dom";
 
