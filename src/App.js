3dee64903d8e62f750eafd45733270f3f847b710
XingYang0827 <xingyang0827@gmail.com>
2023-01-05 15:10:51 -0800

    7-7

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Redirect,
-  Route,
-} from "react-router-dom";
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import { Amplify, Auth, API, Analytics } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import React, { useEffect, useState } from 'react';
+import { Router, Switch, Redirect, Route } from "react-router-dom";
+import { Auth } from 'aws-amplify';
 import { ThemeProvider } from 'styled-components';
-import colors from 'tailwindcss/colors';
 
 import Header from './Header';
 import Search from './Search';
 import Dashboard from './Dashboard';
 import Tool from './Core/Tool';
 import Chat from './Core/Chat';
+import Login from './Login/Login';
 import Profile from './Profile/';
 import LoginSuccess from './Login/Success';
 
 import './App.scss';
-
-Amplify.configure(awsconfig);
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -43,10 +33,42 @@ const App = () => {
     };
     init();
   }, []);
-  
+
   return (
     <ThemeProvider theme={colors}>
-
+      <Router>
+        {redirect ? <Redirect to={redirect} /> : null}
+        {isLoggedIn ? (
+          <>
+            <Switch>
+              <Route path="/writing/document"><div /></Route>
+              <Route component={Header} />
+            </Switch>
+            <Switch>
+              <Route path="/" exact component={Dashboard} />
+              <Route path="/search" exact component={Search} />
+              <Route path="/ai/">
+                <Switch>
+                  <Route path="/ai/code/debugging" component={Chat} />
+                  <Route component={Tool} />
+                </Switch>
+              </Route>
+              <Route path="/my-profile" component={Profile} />
+              <Route path="/signup/failed" component={Profile} />
+              <Route path="/signup/success" component={LoginSuccess} />
+            </Switch>
+          </>
+        ) : (
+          <>
+            <Switch>
+              <Route path="/" exact>
+                <Redirect to="/login" />
+              </Route>
+              <Route path="/" component={Login} />
+            </Switch>
+          </>
+        )}
+      </Router>
     </ThemeProvider>
   );
 };
