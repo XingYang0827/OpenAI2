d1e58b28c8e59281cd1b6c3f5c8975142f1409d6
XingYang0827 <xingyang0827@gmail.com>
2023-01-05 16:37:50 -0800

    9

--- src/App.js
+++ src/App.js
@@ -1,56 +1,81 @@
 
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Redirect,
-  Route,
-} from "react-router-dom";
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import { Amplify, Auth, API, Analytics } from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
+import { Router, Switch, Redirect, Route } from "react-router-dom";
+import { Auth } from 'aws-amplify';
 import { ThemeProvider } from 'styled-components';
-import colors from 'tailwindcss/colors';
-import awsconfig from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
 import Header from './Header';
 import Search from './Search';
 import Dashboard from './Dashboard';
 import Tool from './Core/Tool';
 import Chat from './Core/Chat';
+import Login from './Login/Login';
 import Profile from './Profile/';
 import LoginSuccess from './Login/Success';
+import colors from 'tailwindcss/colors' 
 
 import './App.scss';
 
 Amplify.configure(awsconfig);
 
-@withAuthenticator
-class App extends Component {
-  render() {
-    const user = Auth.currentAuthenticatedUser();
-    return (
-      <ThemeProvider theme={colors}>
-        <Router>
-          {!user ? <Redirect to="/login" /> : null}
-          <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/search" exact component={Search} />
-            <Route path="/my-profile" component={Profile} />
-            <Route path="/signup/failed" component={Profile} />
-            <Route path="/signup/success" component={LoginSuccess} />
-            <Route path="/ai/">
-              <Switch>
-                <Route path="/ai/code/debugging" component={Chat} />
-                <Route component={Tool} />
-              </Switch>
-            </Route>
-            <Route path="/login" component={Auth} />
-            <Route component={Header} />
-          </Switch>
-        </Router>
-      </ThemeProvider>
-    );
-  }
-}
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [profile, setProfile] = useState({});
+  const [redirect, setRedirect] = useState('');
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        if (user) {
+          setProfile(user.attributes);
+          setIsLoggedIn(true);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    init();
+  }, []);
+
+  return (
+    <ThemeProvider theme={colors}>
+      <Router>
+        {redirect ? <Redirect to={redirect} /> : null}
+        {isLoggedIn ? (
+          <>
+            <Switch>
+              <Route path="/writing/document"><div /></Route>
+              <Route component={Header} />
+            </Switch>
+            <Switch>
+              <Route path="/" exact component={Dashboard} />
+              <Route path="/search" exact component={Search} />
+              <Route path="/ai/">
+                <Switch>
+                  <Route path="/ai/code/debugging" component={Chat} />
+                  <Route component={Tool} />
+                </Switch>
+              </Route>
+              <Route path="/my-profile" component={Profile} />
+              <Route path="/signup/failed" component={Profile} />
+              <Route path="/signup/success" component={LoginSuccess} />
+            </Switch>
+          </>
+        ) : (
+          <>
+            <Switch>
+              <Route path="/" exact>
+                <Redirect to="/login" />
+              </Route>
+              <Route path="/" component={Login} />
+            </Switch>
+          </>
+        )}
+      </Router>
+    </ThemeProvider>
+  );
+};
 
 export default withAuthenticator(App);
